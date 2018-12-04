a01fd98d95a9ac2e57edd187dc22088c4e55e894
daBack <oscar.back96@hotmail.com>
2018-12-04 16:16:38 +0100

    Now hides statusbar

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ export default class App extends React.Component {
     render() {
         return (
             <View style={styles.container}>
+                <StatusBar hidden />
                 {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
                 <AppContainer />
             </View>
