db8015413df8a91a34000975db4a6f14c891a624
daBack <oscar.back96@hotmail.com>
2018-12-07 10:59:51 +0100

    Redux: Added store to app.js

--- App.js
+++ App.js
@@ -3,16 +3,21 @@ import { Platform, StatusBar, StyleSheet, Text, View } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import Navbar from './navigation/Navbar';
 
+import store from './redux/store';
+import { Provider } from 'react-redux';
+
 const AppContainer = createAppContainer(Navbar);
 
 export default class App extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
-                <StatusBar hidden />
-                {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-                <AppContainer />
-            </View>
+            <Provider store={store}>
+                <View style={styles.container}>
+                    <StatusBar hidden />
+                    {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+                    <AppContainer />
+                </View>
+            </Provider>
         );
     }
 }
