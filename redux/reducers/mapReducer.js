49e0c44908ffef7a7a950dc7ef18a73ad6ac2198
daBack <oscar.back96@hotmail.com>
2018-12-12 18:59:44 +0100

    Redux: added functions for map reducer and action

--- redux/reducers/mapReducer.js
+++ redux/reducers/mapReducer.js
@@ -1,9 +1,11 @@
 export default function reducer(state = {
     resultArray: [],
+    test: 'Halloj',
 }, action) {
 
     switch (action.type) {
         case 'ADD_RESULT_ARRAY': {
+            
             return { ...state, resultArray: action.payload }
         }
         default: {
