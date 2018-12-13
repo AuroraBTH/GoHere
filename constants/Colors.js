9ebbaee5f0884bf1882d329c455467b057982946
Viktoria Haapaoja <viktoria.haa@hotmail.com>
2018-12-13 13:21:56 +0100

    Added some more colors

--- constants/Colors.js
+++ constants/Colors.js
@@ -2,8 +2,11 @@ const tintColor = '#FEFEFE';
 
 export default {
   tintColor,
+  highlightGrey: '#EEEEEE',
+  borderGrey: '#C9CACB',
   stone: '#406983',
   mist: '#95AEC2',
+  caramel: '#C58243',
   almostBlack: '#030D19',
   tabIconDefault: '#333',
   tabIconSelected: tintColor,
